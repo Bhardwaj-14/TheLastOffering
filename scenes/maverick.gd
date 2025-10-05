de6c0be01e3a2f01ade6204ccf74e046b6fdc5a6
Bhardwaj <bhardwaj.14.mit@gmail.com>
2025-10-05 15:45:48 +0530

    added video

--- scenes/maverick.gd
+++ scenes/maverick.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
 
 @onready var anim = $animation
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 const GRAVITY = 1000.0
@@ -9,16 +8,12 @@ const GRAVITY = 1000.0
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
-	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	velocity.x = input_vector.x * SPEED
-	
 	move_and_slide()
-	
 	var is_attacking = anim.animation == "attack" and anim.is_playing()
 	
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and not is_attacking:
