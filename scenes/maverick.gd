924a9e35265ea5ac80cb7bc93b4607e3021a9f77
Bhardwaj <bhardwaj.14.mit@gmail.com>
2025-10-05 14:05:48 +0530

    Added animations
    hold left mouse btn seems more interactive

--- scenes/maverick.gd
+++ scenes/maverick.gd
@@ -1,25 +1,32 @@
 extends CharacterBody2D
 
+@onready var anim = $animation
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+const GRAVITY = 1000.0
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
+		velocity.y += GRAVITY * delta
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.x = input_vector.x * SPEED
+	
 	move_and_slide()
+	
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		anim.play("attack")
+	elif not is_on_floor():
+		anim.play("jump")
+	elif input_vector.x != 0:
+		anim.play("run")
+	else:
+		anim.play("idle")
+	
+	if input_vector.x != 0:
+		anim.flip_h = input_vector.x < 0
